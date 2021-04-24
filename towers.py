d5f5514241854851dcc34aec53db470a5cb203d3
allya-tt <73088897+allya-tt@users.noreply.github.com>
2021-04-24 23:48:29 +0700

    Update towers.py

--- towers.py
+++ towers.py
@@ -1,6 +1,6 @@
 def towers(n, a, b, c):
     if n == 1:
-        printing(a, b)
+        print("Move ring ", n, ' from ', a, ' rod to ', b, ' rod.', sep='')
     else:
         towers(n-1, a, c, b)
         print("Move ring ", n, ' from ', a, ' rod to ', c, ' rod.', sep='')
@@ -8,8 +8,12 @@ def towers(n, a, b, c):
     return ''
 
 
-n = int(input('Enter the number of the rings: '))   # the number of rings
-a = int(input('Number of the rod, from which is needed to move: '))   # starting rod
-b = int(input('Number of the rod, to where its needed to replace: '))   # finish rod
-c = 6-a-b
-towers(n, a, b, c)
+def main():
+    n = int(input('Enter the number of the rings: '))   # the number of rings
+    a = int(input('Number of the rod, from which is needed to move: '))   # starting rod
+    b = int(input('Number of the rod, to where its needed to replace: '))   # finish rod
+    c = 6-a-b
+    towers(n, a, b, c)
+    
+    
+main()
